d44d642cfe3f336b337cea860e9f3d6d09788184
김주언 <wndjs803@naver.com>
2023-08-26 23:00:01 +0900

    feat: 리뷰 조회 기능

--- Movie/src/main/kotlin/com/example/Movie/review/controller/reviewController.kt
+++ Movie/src/main/kotlin/com/example/Movie/review/controller/reviewController.kt
@@ -1,14 +1,17 @@
 package com.example.Movie.review.controller
 
+import com.example.Movie.movie.domain.Movie
 import com.example.Movie.review.domain.Review
 import com.example.Movie.review.dto.createMovieRequest
 import com.example.Movie.review.dto.createMovieResponse
 import com.example.Movie.review.service.reviewService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -20,5 +23,11 @@ class reviewController(private val reviewService: reviewService){
         val response = createMovieResponse(
             content = createdReview!!.content, grade = createdReview.grade, movie = createdReview.movie.title)
         return ResponseEntity.ok(response)
+    }
+
+    @GetMapping
+    fun findReviews(@RequestParam movieTitle: String): ResponseEntity<List<createMovieResponse>>{
+        val findReviews = reviewService.findReviews(movieTitle)
+        return ResponseEntity.ok(findReviews)
     }
 }
--- Movie/src/main/kotlin/com/example/Movie/review/service/reviewService.kt
+++ Movie/src/main/kotlin/com/example/Movie/review/service/reviewService.kt
@@ -4,7 +4,10 @@ import com.example.Movie.movie.domain.Movie
 import com.example.Movie.movie.repository.movieRepository
 import com.example.Movie.review.domain.Review
 import com.example.Movie.review.dto.createMovieRequest
+import com.example.Movie.review.dto.createMovieResponse
 import com.example.Movie.review.repository.reviewRepository
+import org.hibernate.type.StringNVarcharType
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -23,5 +26,25 @@ class reviewService(val reviewRepository: reviewRepository, val movieRepository:
 
         reviewRepository.save(review)
         return review
+    }
+
+    fun findReviews(movieTitle: String): List<createMovieResponse>{
+        val reviews = reviewRepository.findAll(Sort.by(Sort.Direction.ASC, "createdAt"))
+
+        val filteredReviews = ArrayList<createMovieResponse>()
+
+        for(review in reviews){
+            if(review.movie.title == movieTitle){
+                filteredReviews.add(
+                    createMovieResponse(
+                        content = review.content,
+                        grade = review.grade,
+                        movie = movieTitle
+                    )
+                )
+            }
+        }
+
+        return filteredReviews
     }
 }
