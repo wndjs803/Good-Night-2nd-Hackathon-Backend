98f9e22ef3a9f35940b87a360311c17315e026e2
김주언 <gimjueon@gimjueon-ui-MacBookPro.local>
2023-08-26 11:11:49 +0900

    초기 세팅

---
+++ MovieReview/src/main/kotlin/com/example/MovieReview/MovieReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.example.MovieReview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieReviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieReviewApplication>(*args)
+}
